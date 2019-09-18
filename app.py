b20d1209ae4b40fe146a9f12b6461af4eecea86d
Lee Liao <lee@Lee-MacAir.local>
2019-09-18 21:45:00 +0800

    add code

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, abort
 
 from linebot import (
@@ -11,7 +12,8 @@ from linebot.models import *
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('6f0iOVBzeFhQKMAW2dVqAFgb+FMU5iPqO6wxB+c1m11HIncCXARKrRbPI/YI9W5SDOe6Cs8hbvGvK2CzTPH8ErsBXnRs5UMvC0abhUayHGtXrW/9aLiOh4QcSrJRc57ccQDTV/C8uBy1fiU2Nc4gigdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi(
+    '6f0iOVBzeFhQKMAW2dVqAFgb+FMU5iPqO6wxB+c1m11HIncCXARKrRbPI/YI9W5SDOe6Cs8hbvGvK2CzTPH8ErsBXnRs5UMvC0abhUayHGtXrW/9aLiOh4QcSrJRc57ccQDTV/C8uBy1fiU2Nc4gigdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('a228e3664a186a74e79f9b24c1f4e51b')
 
@@ -34,24 +36,29 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     print(event)
-    text=event.message.text
+    text = event.message.text
 
-    if (text=="Hi" or text=="hi"):
+    if (text == "last pp")
+    with open("released.txt", "r") as f:
+            for line in f:
+                if "pp" in line:
+                    print(line)
+
+    if (text == "Hi" or text == "hi"):
         reply_text = "Hello"
-        #Your user ID
+        # Your user ID
 
-    elif(text=="你好"):
+    elif(text == "你好"):
         reply_text = "哈囉"
-    elif(text=="機器人"):
+    elif(text == "機器人"):
         reply_text = "叫我嗎"
     else:
         reply_text = text
-    
-    
+
     message = TextSendMessage(reply_text)
     line_bot_api.reply_message(event.reply_token, message)
 
-import os
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
